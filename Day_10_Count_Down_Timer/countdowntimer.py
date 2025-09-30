6b16e8f8655058a2964965dd51db7e4f50a1ceb3
Harshit Rathi <harshitrathi321@gmail.com>
2025-09-30 20:27:09 +0530

    Create countdowntimer.py

---
+++ Day_10_Count_Down_Timer/countdowntimer.py
@@ -0,0 +1,13 @@
+import time
+
+# Take input in seconds
+seconds = int(input("Enter time in seconds: "))
+
+while seconds > 0:
+    mins, secs = divmod(seconds, 60)
+    timer = f"{mins:02d}:{secs:02d}"
+    print(timer, end="\r")  # overwrite line
+    time.sleep(1)
+    seconds -= 1
+
+print("⏰ Time's up!")
